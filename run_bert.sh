c2ea6a01a270c50acc51137e14c685a34b4174b5
Marek Żochowski <marek@zochowski.net>
2019-11-09 15:09:44 +0000

    Change tmux to wemux in run_bert.sh

--- run_bert.sh
+++ run_bert.sh
@@ -1,3 +1,3 @@
 SESSION="bert_pretraining"
-tmux new-s -d -s "$SESSION"
-tmux send-keys -t "$SESSION:0" "python3 colab.py" Enter
+wemux new-s -d -s "$SESSION"
+wemux send-keys -t "$SESSION:0" "python3 colab.py" Enter
