018aa6fe1334f10af040572fede8cee51b6174bb
fiffeek <fm394471@students.mimuw.edu.pl>
2019-11-10 17:17:16 +0100

    Remove unused files

--- run_bert.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-SESSION="bert_pretraining"
-wemux new-s -d -s "$SESSION"
-wemux send-keys -t "$SESSION:0" "python3 colab.py" Enter
--- src/bert-experiment/commands.sh
+++ src/bert-experiment/commands.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+
 gsutil cp gs://zpp-bucket-1920/bert-bucket-golkarolka/thms_ls.train .
 gsutil cp gs://zpp-bucket-1920/bert-bucket-golkarolka/vocab_thms_ls.txt .
 mkdir ./shards
 split -a 4 -l 5000 -d thms_ls.train ./shards/shard_
-
 git clone https://github.com/google-research/bert.git
 gsutil cp gs://zpp-bucket-1920/bert-bucket-golkarolka/create_pretraining_data.py bert/
 gsutil cp gs://zpp-bucket-1920/bert-bucket-golkarolka/tokenization.py bert/
