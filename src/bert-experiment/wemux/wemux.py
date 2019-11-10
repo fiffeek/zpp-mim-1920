6b46e1f7a72524567e0184d234ebc7e1e97f8ee0
fiffeek <fm394471@students.mimuw.edu.pl>
2019-11-10 01:40:37 +0100

    Revert

--- src/bert-experiment/wemux/wemux.py
+++ src/bert-experiment/wemux/wemux.py
@@ -1,5 +1,5 @@
+import os
 import logging
-import subprocess
 
 
 tmux_wrapper = "wemux"
@@ -11,23 +11,20 @@ class TmuxSession:
         self.detached = detached
         logging.info("{} Console instantiated, name=[{}]".format(tmux_wrapper, session_name))
 
-    def run_call(self, call):
-        subprocess.check_call(call.split())
-
     def run_wemux_session(self):
         if self.detached:
-            self.run_call("{} new-s -d -s \"{}\"".format(tmux_wrapper, self.session_name))
+            os.system("{} new-s -d -s \"{}\"".format(tmux_wrapper, self.session_name))
         else:
-            self.run_call("{} new-s -s \"{}\"".format(tmux_wrapper, self.session_name))
+            os.system("{} new-s -s \"{}\"".format(tmux_wrapper, self.session_name))
         logging.info("Console name=[{}] is turned on and detached=[{}]".format(self.session_name, self.detached))
         self.info_for_user()
 
     def send_keys(self, command, session_number=0):
-        self.run_call("{} send-keys -t \"{}:{}\" \"{}\" Enter"
-                      .format(tmux_wrapper, self.session_name, session_number, command))
+        os.system("{} send-keys -t \"{}:{}\" \"{}\" Enter"
+                  .format(tmux_wrapper, self.session_name, session_number, command))
 
     def connect_to_session(self):
-        self.run_call(" $ {} attach-session -t {}".format(tmux_wrapper, self.session_name))
+        os.system(" $ {} attach-session -t {}".format(tmux_wrapper, self.session_name))
 
     def info_for_user(self):
         logging.info("To connect to session:")
